14cef41d6a6d2754db57fb43bb14c6011ce230aa
Filip Lobpreis <filob23@student.sdu.dk>
2024-01-12 00:16:35 +0100

    launch: setup launch file
    Signed-off-by: Filip Lobpreis <filob23@student.sdu.dk>

---
+++ src/image_publisher/launch/image_publisher.launch.py
@@ -0,0 +1,23 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+	joy_config = LaunchConfiguration('joy_config', default='ps3')
+	# according to the teleop_twist_joy documentatino the parameter default can be one of: atk3, ps3-holonomic, ps3, xbox, xd3.
+
+	return LaunchDescription([
+		Node(
+			package='dynamixel_sdk_examples',
+			executable='read_write_node',
+			output='screen',
+			emulate_tty=True
+		),
+		Node(
+			package='image_publisher',
+			executable='img_pub',
+			output='screen',
+			emulate_tty=True
+		)
+	])
+
