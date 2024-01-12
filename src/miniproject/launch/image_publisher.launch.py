84c86f079dc1bdf34be33c35bcf4890532645977
Filip Lobpreis <filob23@student.sdu.dk>
2024-01-12 17:00:26 +0100

    launch: remove unused code
    Signed-off-by: Filip Lobpreis <filob23@student.sdu.dk>

--- src/miniproject/launch/image_publisher.launch.py
+++ src/miniproject/launch/image_publisher.launch.py
@@ -1,11 +1,7 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
-	joy_config = LaunchConfiguration('joy_config', default='ps3')
-	# according to the teleop_twist_joy documentatino the parameter default can be one of: atk3, ps3-holonomic, ps3, xbox, xd3.
-
 	return LaunchDescription([
 		Node(
 			package='dynamixel_sdk_examples',
